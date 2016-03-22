8b8d077b2d37b94e4c4f7a48ac896268e1ea86df
aurceramartins <alexurcera23@gmail.com>
2016-03-22 17:58:38 +0100

    primer comit tema odoo

---
+++ theme_Mica/__openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'Tutorial themeMica',
+  'description': 'Mica Profe.',
+  'version':'1.0',
+  'author':'Urcera',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
